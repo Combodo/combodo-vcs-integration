c9742f9f3a0a3c7c3c06f56412356f8bc0e8323d
Benjamin Dalsass <95754414+bdalsass@users.noreply.github.com>
2024-08-08 10:00:44 +0200

    GitHub on support combodo need itop structure dependency

--- module.combodo-vcs-integration.php
+++ module.combodo-vcs-integration.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-vcs-integration/1.0.1',
+	'combodo-vcs-integration/1.0.2',
 	array(
 		// Identification
 		//
@@ -15,7 +15,7 @@ SetupWebPage::AddModule(
 		// Setup
 		//
 		'dependencies' => array(
-			'itop-config-mgmt/3.1.1',
+			'itop-structure/3.1.1',
 		),
 		'mandatory' => false,
 		'visible' => true,
