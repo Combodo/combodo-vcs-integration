282f1d6f60247fdd74f0792fd455b56053fd7797
Benjamin Dalsass <95754414+bdalsass@users.noreply.github.com>
2024-08-26 16:01:03 +0200

    combodo-vcs-integration 1.0.5

--- module.combodo-vcs-integration.php
+++ module.combodo-vcs-integration.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-vcs-integration/1.0.3',
+	'combodo-vcs-integration/1.0.5',
 	array(
 		// Identification
 		//
